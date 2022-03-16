3a4f5130ccb6de7b4be4fa9ed2bc928fb27a1833
StandardDao <101048151+StandardDao@users.noreply.github.com>
2022-03-16 19:47:03 +0100

    Update and rename StokenNft to Dan.sol

--- Dan.sol
+++ Dan.sol
@@ -5,12 +5,12 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract StokenNft is ERC721, Ownable {
+contract StandardDao is ERC721, Ownable {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
 
-    constructor() ERC721("StokenNft", "STK") {}
+    constructor() ERC721("StandardDao", "DAN") {}
 
     function safeMint(address to) public onlyOwner {
         uint256 tokenId = _tokenIdCounter.current();
